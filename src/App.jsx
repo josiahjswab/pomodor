3c21ce2037b40738df536c82dc6e21b990501e50
Josiah Swab <josiahjswab@gmail.com>
2021-01-23 09:25:33 -0800

    adds pausing and resuming functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,49 @@ export default function App() {
 
   const [time, setTime] = useState(25);
   const [timeRemaining, setTimeRemaining] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
 
   const seconds = time * 60;
 
   const countDown = () => {
-    setTimeRemaining(seconds)
+    isPaused ? [setIsPaused(false), setTimeRemaining(timeRemaining - 1)] : setTimeRemaining(seconds);
+    // The above code is the below refactored.
+    // if(isPaused === false) {
+    //   setTimeRemaining(seconds)
+    // } else {
+    //   setIsPaused(false);
+    //   setTimeRemaining(timeRemaining - 1)
+    // }
   };
 
   const minutesRemaining = Math.floor(timeRemaining/60)
   const secondsRemaining = timeRemaining % 60
-
-  const stopCount = () => {
-    setTimeRemaining(0)
-  }
-
+  
   const TimerDisplay = () => {
     let result = {};
     if(minutesRemaining < 10) {
       result.minutesRemaining = `0${minutesRemaining}`
     } else {result.minutesRemaining = minutesRemaining}
-
+    
     if(secondsRemaining < 10) {
       result.secondsRemaining = `0${secondsRemaining}`
     } else {result.secondsRemaining = secondsRemaining}
     return <h1>{result.minutesRemaining}:{result.secondsRemaining}</h1>
   }
+  
+  let intervel = null;
+
+  const pauseCount = () => {
+    setIsPaused(true);
+    clearInterval(intervel);
+  }
+
+  const stopCount = () => {
+    setTimeRemaining(0)
+    // clearInterval(intervel) //use this to create pause feature.
+  }
 
   useEffect(() => {
-    let intervel = null;
     const timerNoise = document.getElementsByClassName('audio-win')[0]
 
     if(timeRemaining <= 0) {
@@ -57,6 +72,7 @@ export default function App() {
         onChange={(e) => setTime(e.target.value)}
       />
       <button onClick={countDown}>Start</button>
+      <button onClick={pauseCount}>II</button>
       <button onClick={stopCount}>Stop</button>
       <TimerDisplay/>
       <audio className='audio-win'>
